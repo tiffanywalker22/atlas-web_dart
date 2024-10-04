87b1a96681c7a95ed1f7b66200c0028290f8fa85
tiffany <tbcasper16@gmail.com>
2024-10-04 16:34:38 -0500

    outer inner task 4

---
+++ 0x01-dart-function/4-outer_inner.dart
@@ -0,0 +1,8 @@
+void outer(String name, String id) {
+  print(inner(name, id));
+}
+
+String inner(String name, String id) {
+  List<String> parts = name.split('');
+  return 'Hello Agent B.${parts[0]} your id is $id';
+}
