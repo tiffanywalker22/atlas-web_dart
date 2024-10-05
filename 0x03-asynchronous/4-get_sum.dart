ab3e71fbf7e52f5ef8b1d2f82d6672b84c13ec74
tiffany <tbcasper16@gmail.com>
2024-10-04 21:40:51 -0500

    quick fix

--- 0x03-asynchronous/4-get_sum.dart
+++ 0x03-asynchronous/4-get_sum.dart
@@ -5,9 +5,13 @@ import '4-util.dart';
 Future<double> calculateTotal() async {
   try {
     var user = json.decode(await fetchUserData());
-    var orders = json.decode(await fetchUserOrders(user['id']));
+    var orders = json.decode(await fetchUserOrders(user['id'])) ?? [];
 
     double totalPrice = 0.0;
+
+    if (orders.isEmpty) {
+      return 0.0;
+    }
 
     for (var product in orders) {
       var productPrice = json.decode(await fetchProductPrice(product));
