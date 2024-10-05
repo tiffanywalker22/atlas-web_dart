324b7ffffb5260a9566123bd5dcef13eeae9173d
tiffany <tbcasper16@gmail.com>
2024-10-04 21:32:22 -0500

    task four calcul total

---
+++ 0x03-asynchronous/4-get_sum.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+import '4-util.dart';
+
+
+Future<double> calculateTotal() async {
+  try {
+    var user = json.decode(await fetchUserData());
+    var orders = json.decode(await fetchUserOrders(user['id']));
+
+    double totalPrice = 0.0;
+
+    for (var product in orders) {
+      var productPrice = json.decode(await fetchProductPrice(product));
+      totalPrice += productPrice ?? 0.0;
+    }
+
+    return totalPrice;
+  } catch (error) {
+    print('Error: $error');
+    return -1;
+  }
+}
+
